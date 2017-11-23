016f620d89b49a91d7286fb428e156658aa9fd95
Lucie Keunen <lucie@keunen.net>
2017-11-23 15:17:51 +0100

    Defined new create and destroy methods in doses controller and new view

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,31 @@
 class DosesController < ApplicationController
   def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @ingredient = Ingredient.find_by(name: params[:dose]["ingredient"])
+    @dose.ingredient_id = @ingredient.id
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail_id = @cocktail.id
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
   end
 end
